59a4b4e859f6cb54e0fab5cba61c7b0e8b021139
vsrn09 <venkat.namala@ibm.com>
2019-12-18 05:56:36 -0600

    Refactored

---
+++ app/index.aspx
@@ -0,0 +1,12 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="~/index.aspx.cs" Inherits="WebApplication_dotNet.app.index" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+     <asp:Label ID ="lbl_test" runat="server"></asp:Label>
+</body>
+</html>
---
+++ app/index.aspx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace WebApplication_dotNet.app
+{
+    public partial class index : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //CONNECT TO DB
+
+            string connStr = "Server=localhost;Database=test;UID=sa;PWD=admin12345678";
+            //string connStr = "Server=mssql-2017-mssqldemo.cpat-cpak4mcm-cluster-afb9c6047b062b44e3f1b3ecfeba4309-0002.us-south.containers.appdomain.cloud;Database=test;UID=sa;PWD=admin12345678";
+            SqlConnection conn = new SqlConnection(connStr);
+            conn.Open();
+
+            //CREATE A COMMAND
+            SqlCommand cmd = new SqlCommand("SELECT [id],[name] FROM [dbo].[test_tab]");
+            cmd.CommandType = System.Data.CommandType.Text;
+            cmd.Connection = conn;
+            string temp = "";
+            //READ FROM DB
+            SqlDataReader reader = cmd.ExecuteReader();
+            while(reader.Read())
+            {
+                temp += reader["id"].ToString();
+                temp += reader["name"].ToString();
+                temp += "<br/>";
+
+            }
+            conn.Close();
+            lbl_test.Text = temp;
+
+        }
+    }
+}
